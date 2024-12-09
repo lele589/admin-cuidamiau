6a32ba8f0d3cbfb19f028cff765f8a1d8f787ff4
elenacarrasco <elena.carrasco@zinklar.com>
2024-12-09 20:19:03 +0100

    fix: update migrations path

--- knexfile.ts
+++ knexfile.ts
@@ -1,9 +1,13 @@
 import { Knex } from 'knex';
+import path from 'path';
 
 const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
     filename: './dev.sqlite3',
+  },
+  migrations: {
+    directory: path.join(__dirname, 'src/database/migrations'),
   },
   useNullAsDefault: true, // Needed for SQLite
 };
